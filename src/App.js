ac61a509776201bcc7ff05e8c3dc7b413c592dff
aerooneqq <aerooneq@yandex.ru>
2019-07-16 02:22:39 +0300

    changed size of an user activity element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomeComponent from "./components/mainComponents/HomeComponent"
 import WorkplaceComponent from "./components/mainComponents/WorkplaceComponent"
 
